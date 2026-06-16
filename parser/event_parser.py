1c85329954fd104514a8cda4359233049ae57d5f
Shaleen Garg <shaleengarg15@gmail.com>
2026-06-16 17:57:54 +0530

    function implemented

--- parser/event_parser.py
+++ parser/event_parser.py
@@ -1,34 +1,39 @@
 from Evtx.Evtx import Evtx
 import xml.etree.ElementTree as ET
 
+
+def get_sys_data(root):
+
+    event_id = root.find(".//e:EventID", ns).text
+
+    time = root.find(".//e:TimeCreated", ns).attrib["SystemTime"]
+
+    computer = root.find(".//e:Computer", ns).text
+
+    return event_id, time, computer
+
+
+ns = {
+    "e": "http://schemas.microsoft.com/win/2004/08/events/event"
+}
+
+interesting_ids = {"4624", "4625", "4720", "4740"}
+
+
 with Evtx(r"tests/sample_logs/logs1.evtx") as log:
 
     for record in log.records():
 
-        print(record.xml())
+        root = ET.fromstring(record.xml())
 
-        break
-    ns={
-        "e":"http://schemas.microsoft.com/win/2004/08/events/event"
-    }
-    root = ET.fromstring(record.xml())
-    print(root.tag)
-    event_id=root.find(".//e:EventID",ns)
-    print(event_id.text)
-    time=root.find(".//e:TimeCreated",ns)
-    print(time.attrib["SystemTime"])
-    computer=root.find(".//e:Computer",ns)
-    print(computer.text)
-    for item in root.findall(".//e:Data",ns):
-        print(item.attrib)
-        print(item.text)
+        event_id, time, computer = get_sys_data(root)
 
-    def get_data(root,name):
-        for item in root.findall(".//e:Data",ns):
-            if item.attrib.get("Name")==name:
-                return item.text
-        return None
-    
+        if event_id in interesting_ids:
 
-username = get_data(root,"TargetUserName")
-print(username)
+            print()
+
+            print("Event:", event_id)
+
+            print("Time:", time)
+
+            print("Computer:", computer)
